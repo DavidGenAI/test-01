06b034dc896e4d3d94743a3753af7dfcfe21d068
DavidGenAI <147370126+DavidGenAI@users.noreply.github.com>
2024-12-01 04:22:32 -0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCIZ74FPVXps5XfCldIb42-UBuTbQhOJV0",
+  authDomain: "world-shorts.firebaseapp.com",
+  projectId: "world-shorts",
+  storageBucket: "world-shorts.firebasestorage.app",
+  messagingSenderId: "299276936060",
+  appId: "1:299276936060:web:b33d605263f7f5b5d37af9",
+  measurementId: "G-XZFKCVMPV7"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
