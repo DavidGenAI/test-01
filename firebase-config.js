a55478aa6da0eb62dc385e12de93aa24ec133c2d
DavidGenAI <147370126+DavidGenAI@users.noreply.github.com>
2024-12-01 05:22:09 -0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,6 +7,3 @@ const firebaseConfig = {
   appId: "1:299276936060:web:b33d605263f7f5b5d37af9",
   measurementId: "G-XZFKCVMPV7"
 };
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
